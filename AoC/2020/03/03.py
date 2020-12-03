470b6b1a917999573ef5b91be60c007e0624f86b
Petre Leonard Macamete <petreleonardos@gmail.com>
2020-12-03 13:53:04 +0200

    day 3

--- AoC/2020/03/03.py
+++ AoC/2020/03/03.py
@@ -3,14 +3,14 @@ lines = file_opened.readlines()
 from parse import *
 right_step = 3
 down_step = 1
-init_y = 0
-init_x = 0
+y = 0
+x = 0
 trees = 0
 
-while init_y < len(lines):
-    if lines[init_y][init_x] == '#':
+while y < len(lines):
+    if lines[y][x] == '#':
         trees += 1
-    init_y += down_step
-    init_x += right_step
-    init_x = init_x %  (len(lines[0]) - 1)
+    y += down_step
+    x += right_step
+    x = x %  (len(lines[0]) - 1)
 print(trees)
