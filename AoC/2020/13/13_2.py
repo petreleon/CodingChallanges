0772af5437b03214218725b03ae559e1acd01fe6
Petre Leonard Macamete <petreleonardos@gmail.com>
2020-12-13 15:57:25 +0200

    numpy solution

--- AoC/2020/13/13_2.py
+++ AoC/2020/13/13_2.py
@@ -1,7 +1,6 @@
 from parse import parse
 from operator import methodcaller
-import sympy as sym
-x = sym.symbols('x')
+from sympy.ntheory.modular import crt
 
 file_opened = open("input", 'r')
 lines = file_opened.read().splitlines()
@@ -18,11 +17,18 @@ found = False
 timeStamp = 0
 lcm = busIDs[0][0]
 
+EquationsMod = [busIDs[0][0]]
+EquationsEqual = [0]
+
 print("Mod[x,{}]=={}".format(busIDs[0][0], 0), end='')
 iterated = 1 + busIDs[0][1]
 for index,iteratedID in enumerate(busIDs[1:]):
     print("&& Mod[x,{}]=={}".format(iteratedID[0], (-iterated)%iteratedID[0]), end="")
+    EquationsMod.append(iteratedID[0])
+    EquationsEqual.append((-iterated)%iteratedID[0])
     iterated += iteratedID[1] + 1
 print('')
+solution = crt(EquationsMod, EquationsEqual)
+print(solution[0])
 # the output used with solve from wolfram mathematica
 # https://reference.wolfram.com/language/ref/Solve.html
